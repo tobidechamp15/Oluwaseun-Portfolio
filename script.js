b265c1931f02f24543e99b5ba1f1865b3d570089
unknown <tobidechamp15@gmail.com>
2023-07-10 11:59:56 +0100

    working non-stop

---
+++ script.js
@@ -0,0 +1,9 @@
+const name = document.querySelector("#name");
+const email = document.querySelector("#email");
+const textArea = document.querySelector("#textArea");
+
+function formValidation(e) {
+    e.preventDefault();
+
+    
+}
